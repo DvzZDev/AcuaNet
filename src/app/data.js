13542f325759aa2549db4f85669d74968256a65c
David <estebandavid578@gmail.com>
2024-06-08 20:34:35 +0200

    chore: Add FetchEsp API function for fetching ESP data

--- src/app/data.js
+++ src/app/data.js
@@ -18,3 +18,10 @@ export async function FetchEmbalsesVariacion() {
   const data = await response.json()
   return data
 }
+
+export async function FetchEsp() {
+  const url = 'https://api-acua-production.up.railway.app/api/esp'
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
+}

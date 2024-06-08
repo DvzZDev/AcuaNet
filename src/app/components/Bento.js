dbb99184dad6f34d6236e6cde952fa48bae8e25a
David <estebandavid578@gmail.com>
2024-06-08 20:00:23 +0200

    Update Bento component with cuenca and embalses variations

--- src/app/components/Bento.js
+++ src/app/components/Bento.js
@@ -1,18 +1,25 @@
-import { FetchCuencas } from '../data'
+import { FetchCuencas, FetchCuencaVariacion, FetchEmbalsesVariacion } from '../data'
+import '@fontsource-variable/comfortaa'
+
 async function Bento() {
   const cuencas = await FetchCuencas()
+  const variacionCuencas = await FetchCuencaVariacion()
+  const variacionEmbalses = await FetchEmbalsesVariacion()
+
   return (
     <section
       id="bento"
       className="flex h-screen content-center items-center justify-center"
     >
-      <div className="flex h-[80svh] w-[20rem] flex-col justify-center gap-4 lg:grid lg:w-[60rem] lg:grid-cols-10 lg:grid-rows-2">
-        <div className="col-span-4 row-span-1 text-wrap rounded-lg outline outline-slate-500 backdrop-blur-md lg:col-span-4"></div>
-        <div className="col-span-6 row-span-1 min-h-full min-w-full overflow-hidden rounded-lg outline outline-slate-500 backdrop-blur-md">
-          <div className="grid min-h-full min-w-full grid-cols-2 grid-rows-4 gap-2 p-2 text-center md:grid-cols-3 lg:grid-cols-4">
+      <div className="flex h-[80svh] w-[20rem] flex-col justify-center gap-4 lg:grid lg:h-[85svh] lg:w-[60rem] lg:grid-cols-10 lg:grid-rows-2">
+        <div className="col-span-4 row-span-1 text-wrap rounded-lg backdrop-blur-xl lg:col-span-4">
+          COL 1
+        </div>
+        <div className="0 col-span-6 row-span-1 min-h-full min-w-full content-center justify-center overflow-hidden rounded-lg p-2 backdrop-blur-xl">
+          <div className="grid h-full w-full grid-cols-2 grid-rows-8 gap-2 text-center md:grid-cols-3 md:grid-rows-4 lg:grid-cols-4 ">
             {cuencas.map((cuenca) => (
               <div
-                className={`content-center overflow-hidden whitespace-normal rounded-md bg-opacity-50 p-1 text-sm ${getColor(cuenca.porcentaje_embalsada)}`}
+                className={`content-center overflow-hidden whitespace-normal rounded-md bg-opacity-50 p-1 text-sm hover:scale-110 transition-all ${getColor(cuenca.porcentaje_embalsada)}`}
                 key={cuenca.id_cuenca}
               >
                 <div>
@@ -23,12 +30,45 @@ async function Bento() {
             ))}
           </div>
         </div>
-        <div className="col-span-5 rounded-lg outline outline-slate-500 backdrop-blur-md">
-          Columna 3
+
+        <div className="col-span-5 rounded-lg backdrop-blur-xl">
+          <h1 className="pt-2 text-center text-xl font-bold text-green-200">
+            Mayor Variacion Semanal
+          </h1>
+          <div className="p-5">
+            <table className="m-auto w-[70%] table-auto">
+              <caption className="font-bold italic text-green-200">CUENCA</caption>
+              <tbody>
+                {variacionCuencas.map((variacion) => (
+                  <tr
+                    className="transition-all hover:scale-125 hover:text-[#fbf1d2]"
+                    key={variacion.cuenca}
+                  >
+                    <td>{variacion.cuenca.replace(/_/g, ' ')}</td>
+                    <td className="text-right">{`${variacion.porcentaje_variacion} %`}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            <table className="m-auto w-[70%] table-auto">
+              <caption className="font-bold italic text-green-200">EMBALSE</caption>
+              <tbody>
+                {variacionEmbalses.map((variacion) => (
+                  <tr
+                    className="transition-all hover:scale-125 hover:text-[#fbf1d2]"
+                    key={variacion.nombre_embalse}
+                  >
+                    <td>{variacion.nombre_embalse}</td>
+                    <td className="text-right">{`${variacion.variacion_ultima_semanapor} %`}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div className="col-span-5 rounded-lg outline outline-slate-500 backdrop-blur-md">
-          Columna 4
-        </div>
+
+        <div className="col-span-5 rounded-lg backdrop-blur-xl">Columna 4</div>
       </div>
     </section>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Intro from '@/app/components/Intro'
 import Header from './components/Header'
 import Bento from './components/Bento'
 
+
 export default async function Home() {
   return (
     <>

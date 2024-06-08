5001d5d0a3527eb53b74216715c95752916d57a6
David <estebandavid578@gmail.com>
2024-06-08 19:59:52 +0200

    Add API functions for fetching cuenca and embalses variations

--- src/app/data.js
+++ src/app/data.js
@@ -4,3 +4,17 @@ export async function FetchCuencas() {
   const data = await response.json()
   return data
 }
+
+export async function FetchCuencaVariacion() {
+  const url = 'https://api-acua-production.up.railway.app/api/variacion/cuencas'
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
+}
+
+export async function FetchEmbalsesVariacion() {
+  const url = 'https://api-acua-production.up.railway.app/api/variacion/embalses'
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
+}

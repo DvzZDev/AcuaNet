b42235bf5f346488dc6011aa447b8d09867e17f2
David <estebandavid578@gmail.com>
2024-06-08 21:54:29 +0200

    refactor: Update Bento component with cuenca and embalses variations

--- src/app/components/Bento.js
+++ src/app/components/Bento.js
@@ -1,25 +1,59 @@
-import { FetchCuencas, FetchCuencaVariacion, FetchEmbalsesVariacion } from '../data'
 import '@fontsource-variable/comfortaa'
+import Piramid from './Piramid'
+import {
+  FetchCuencas,
+  FetchCuencaVariacion,
+  FetchEmbalsesVariacion,
+  FetchEsp,
+} from '../data'
+
+function getColor(porcentaje) {
+  if (porcentaje >= 90) {
+    return 'bg-blue-500 text-white font-bold  '
+  } else if (porcentaje >= 70) {
+    return 'bg-green-500 text-white font-bold  '
+  } else if (porcentaje >= 50) {
+    return 'bg-yellow-500 text-white font-bold  '
+  } else if (porcentaje >= 30) {
+    return 'bg-orange-500 text-white font-bold  '
+  } else {
+    return 'bg-red-500 text-white font-bold  '
+  }
+}
 
 async function Bento() {
   const cuencas = await FetchCuencas()
   const variacionCuencas = await FetchCuencaVariacion()
   const variacionEmbalses = await FetchEmbalsesVariacion()
+  const esp = await FetchEsp()
 
   return (
     <section
       id="bento"
-      className="flex h-screen content-center items-center justify-center"
+      className="flex h-[200svh] content-center items-center justify-center lg:h-[100svh]"
     >
-      <div className="flex h-[80svh] w-[20rem] flex-col justify-center gap-4 lg:grid lg:h-[85svh] lg:w-[60rem] lg:grid-cols-10 lg:grid-rows-2">
-        <div className="col-span-4 row-span-1 text-wrap rounded-lg backdrop-blur-xl lg:col-span-4">
-          COL 1
+      <div className="relative flex h-[80svh] w-[20rem] flex-col justify-center gap-4 lg:grid lg:h-[90svh] lg:w-[60rem] lg:grid-cols-10 lg:grid-rows-2">
+        <div className="relative z-10 col-span-4 row-span-1 flex items-center justify-center text-wrap rounded-lg backdrop-blur-xl lg:col-span-4">
+          {esp.map((españa) => (
+            <div
+              key={españa.id}
+              className="content-center justify-center rounded-lg bg-opacity-50 text-center backdrop-blur-sm"
+            >
+              <p
+                className={`m-0 p-0 text-[8rem] ${españa.porcentaje_embalsado > 50 ? 'text-green-500' : 'text-red-500'}`}
+              >
+                {`${españa.porcentaje_embalsado}`}
+                <strong className="text-[5rem]">%</strong>
+              </p>
+              <h2 className="text-[2rem] text-blue-200">Reserva Nacional de Agua</h2>
+            </div>
+          ))}
         </div>
         <div className="0 col-span-6 row-span-1 min-h-full min-w-full content-center justify-center overflow-hidden rounded-lg p-2 backdrop-blur-xl">
-          <div className="grid h-full w-full grid-cols-2 grid-rows-8 gap-2 text-center md:grid-cols-3 md:grid-rows-4 lg:grid-cols-4 ">
+          <div className="grid h-full w-full grid-cols-2 grid-rows-8 gap-2 text-center md:grid-cols-3 md:grid-rows-4 lg:grid-cols-4">
             {cuencas.map((cuenca) => (
               <div
-                className={`content-center overflow-hidden whitespace-normal rounded-md bg-opacity-50 p-1 text-sm hover:scale-110 transition-all ${getColor(cuenca.porcentaje_embalsada)}`}
+                className={`content-center overflow-hidden whitespace-normal rounded-md bg-opacity-50 p-1 text-sm transition-all hover:scale-110 ${getColor(cuenca.porcentaje_embalsada)}`}
                 key={cuenca.id_cuenca}
               >
                 <div>
@@ -32,12 +66,14 @@ async function Bento() {
         </div>
 
         <div className="col-span-5 rounded-lg backdrop-blur-xl">
-          <h1 className="pt-2 text-center text-xl font-bold text-green-200">
+          <h1 className="pt-5 text-center text-xl font-bold text-green-200">
             Mayor Variacion Semanal
           </h1>
-          <div className="p-5">
+          <div className="p-2">
             <table className="m-auto w-[70%] table-auto">
-              <caption className="font-bold italic text-green-200">CUENCA</caption>
+              <caption className="text-left font-bold italic text-green-200">
+                CUENCA
+              </caption>
               <tbody>
                 {variacionCuencas.map((variacion) => (
                   <tr
@@ -52,7 +88,9 @@ async function Bento() {
             </table>
 
             <table className="m-auto w-[70%] table-auto">
-              <caption className="font-bold italic text-green-200">EMBALSE</caption>
+              <caption className="p-1 text-left font-bold italic text-green-200">
+                EMBALSE
+              </caption>
               <tbody>
                 {variacionEmbalses.map((variacion) => (
                   <tr
@@ -68,23 +106,10 @@ async function Bento() {
           </div>
         </div>
 
-        <div className="col-span-5 rounded-lg backdrop-blur-xl">Columna 4</div>
+        <div className="col-span-5 rounded-lg backdrop-blur-xl"></div>
       </div>
     </section>
   )
-}
-function getColor(porcentaje) {
-  if (porcentaje >= 90) {
-    return 'bg-blue-500 text-white font-bold  '
-  } else if (porcentaje >= 70) {
-    return 'bg-green-500 text-white font-bold  '
-  } else if (porcentaje >= 50) {
-    return 'bg-yellow-500 text-white font-bold  '
-  } else if (porcentaje >= 30) {
-    return 'bg-orange-500 text-white font-bold  '
-  } else {
-    return 'bg-red-500 text-white font-bold  '
-  }
 }
 
 export default Bento

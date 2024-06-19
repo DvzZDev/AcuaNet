147a41ba25480b1d3fb6bfd921928740af640874
David <estebandavid578@gmail.com>
2024-06-19 02:45:38 +0200

    Update Bento component font size for different percentage ranges

--- src/app/components/Bento.js
+++ src/app/components/Bento.js
@@ -9,15 +9,15 @@ import {
 
 function getColor(porcentaje) {
   if (porcentaje >= 80) {
-    return 'bg-blue-200 text-blue-900 font-bold text-[17px]'
+    return 'bg-blue-200 text-blue-900 font-bold text-[18px]'
   } else if (porcentaje >= 60) {
-    return 'bg-green-200 text-green-900 font-bold text-[17px]'
+    return 'bg-green-200 text-green-900 font-bold text-[18px]'
   } else if (porcentaje >= 40) {
-    return 'bg-yellow-200 text-yellow-900 font-bold text-[17px]'
+    return 'bg-yellow-200 text-yellow-900 font-bold text-[18px]'
   } else if (porcentaje >= 20) {
-    return 'bg-orange-200 text-orange-950 font-bold text-[17px]'
+    return 'bg-orange-200 text-orange-950 font-bold text-[18px]'
   } else {
-    return 'bg-red-200 text-red-900 font-bold text-[17px]'
+    return 'bg-red-200 text-red-900 font-bold text-[18px]'
   }
 }
 

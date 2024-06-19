6f4defd9de9cd83fd6ef5a2323284bc8ef3ef911
David <estebandavid578@gmail.com>
2024-06-20 01:38:32 +0200

    refactor: Improve Bento component styling and layout

--- src/app/components/Bento.js
+++ src/app/components/Bento.js
@@ -28,7 +28,7 @@ async function Bento() {
   const esp = await FetchEsp()
   const pluvis = await FetchPluvis()
 
-   return (
+  return (
     <section
       id="bento"
       className="flex h-full content-center items-center justify-center lg:h-[100svh]"

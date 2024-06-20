09b657c26bc96b0f681489ca7e323dcaab0e9e68
David <estebandavid578@gmail.com>
2024-06-20 03:28:02 +0200

    refactor: Remove unnecessary cache control query parameter in API requests

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -1,6 +1,6 @@
 export async function FetchCuencas() {
   const url = 'https://api-acua-production.up.railway.app/api/cuencas'
-  const response = await fetch(url + '?t=' + Date.now(), {
+  const response = await fetch(url, {
     headers: {
       'Cache-Control': 'no-cache, no-store, must-revalidate',
       Pragma: 'no-cache',
@@ -13,7 +13,7 @@ export async function FetchCuencas() {
 
 export async function FetchCuencaVariacion() {
   const url = 'https://api-acua-production.up.railway.app/api/variacion/cuencas'
-  const response = await fetch(url + '?t=' + Date.now(), {
+  const response = await fetch(url, {
     headers: {
       'Cache-Control': 'no-cache, no-store, must-revalidate',
       Pragma: 'no-cache',
@@ -26,7 +26,33 @@ export async function FetchCuencaVariacion() {
 
 export async function FetchEmbalsesVariacion() {
   const url = 'https://api-acua-production.up.railway.app/api/variacion/embalses'
-  const response = await fetch(url + '?t=' + Date.now(), {
+  const response = await fetch(url, {
+    headers: {
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+    },
+  })
+  const data = await response.json()
+  return data
+}
+
+export async function FetchEsp() {
+  const url = 'https://api-acua-production.up.railway.app/api/esp'
+  const response = await fetch(url, {
+    headers: {
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+    },
+  })
+  const data = await response.json()
+  return data
+}
+
+export async function FetchPluvis() {
+  const url = 'https://api-acua-production.up.railway.app/Api/pluvis'
+  const response = await fetch(url, {
     headers: {
       'Cache-Control': 'no-cache, no-store, must-revalidate',
       Pragma: 'no-cache',

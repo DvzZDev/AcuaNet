6355c336a86dda9bc8f848c7a688c4315c821434
David <estebandavid578@gmail.com>
2024-06-20 03:20:31 +0200

    refactor: Add Cache-Control headers to API requests for no-cache, no-store, must-revalidate

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -1,8 +1,10 @@
 export async function FetchCuencas() {
   const url = 'https://api-acua-production.up.railway.app/api/cuencas'
-  const response = await fetch(url, {
+  const response = await fetch(url + '?t=' + Date.now(), {
     headers: {
-      'Cache-Control': 'no-cache',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
     },
   })
   const data = await response.json()
@@ -11,9 +13,11 @@ export async function FetchCuencas() {
 
 export async function FetchCuencaVariacion() {
   const url = 'https://api-acua-production.up.railway.app/api/variacion/cuencas'
-  const response = await fetch(url, {
+  const response = await fetch(url + '?t=' + Date.now(), {
     headers: {
-      'Cache-Control': 'no-cache',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
     },
   })
   const data = await response.json()
@@ -22,31 +26,11 @@ export async function FetchCuencaVariacion() {
 
 export async function FetchEmbalsesVariacion() {
   const url = 'https://api-acua-production.up.railway.app/api/variacion/embalses'
-  const response = await fetch(url, {
+  const response = await fetch(url + '?t=' + Date.now(), {
     headers: {
-      'Cache-Control': 'no-cache',
-    },
-  })
-  const data = await response.json()
-  return data
-}
-
-export async function FetchEsp() {
-  const url = 'https://api-acua-production.up.railway.app/api/esp'
-  const response = await fetch(url, {
-    headers: {
-      'Cache-Control': 'no-cache',
-    },
-  })
-  const data = await response.json()
-  return data
-}
-
-export async function FetchPluvis() {
-  const url = 'https://api-acua-production.up.railway.app/Api/pluvis'
-  const response = await fetch(url, {
-    headers: {
-      'Cache-Control': 'no-cache',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
     },
   })
   const data = await response.json()

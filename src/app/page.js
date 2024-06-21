903160c4d5748fdfed50ddf116b0d01a3bdf349b
David <estebandavid578@gmail.com>
2024-06-21 03:34:51 +0200

    refactor: Remove unused Header and Footer components from Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import Header from '@/app/components/Header'
 import Intro from '@/app/components/Intro'
 import Bento from '@/app/components/Bento'
-import Footer from './components/Footer'
 import Fuentes from '@/app/components/Fuentes'
 import SkeletonBento from './skeletrons/BentoSkeleton'
 import { Suspense } from 'react'
@@ -13,13 +11,11 @@ export const fetchCache = 'force-no-store'
 export default function Home() {
   return (
     <>
-      <Header />
       <Intro />
       <Suspense fallback={<SkeletonBento />}>
         <Bento />
       </Suspense>
       <Fuentes />
-      <Footer />
     </>
   )
 }

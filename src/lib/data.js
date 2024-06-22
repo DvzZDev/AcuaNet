1725ac472cdde1c4b2bacfcb7ba6942dcdf682c4
David <estebandavid578@gmail.com>
2024-06-22 13:28:51 +0200

    Add FetchEmbalses function to data.js

--- src/lib/data.js
+++ src/lib/data.js
@@ -59,3 +59,10 @@ export async function FetchPluvis() {
   const data = await response.json()
   return data
 }
+
+export async function FetchEmbalses() {
+  const url = 'https://api-acua-production.up.railway.app/Api/embalses'
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
+}

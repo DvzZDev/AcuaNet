4b15290d1fa78628c7358f8c5c49a79121f5d2b5
David <estebandavid578@gmail.com>
2024-06-22 15:38:15 +0200

    Add SkeletonCuencas component for loading state

---
+++ src/components/cuencas/SkeletonCuencas.js
@@ -0,0 +1,23 @@
+async function bento() {
+  return (
+    <section className="flex h-full items-center justify-center bg-[#070922]">
+      <div className="m-10 grid min-h-[45rem] w-[70%] grid-cols-1 grid-rows-none gap-4 text-center md:grid-cols-2 lg:grid-cols-4">
+        {Array.from({ length: 16 }).map((_, index) => (
+          <div
+            className={`flex h-full flex-col content-center items-center justify-center whitespace-normal rounded-md bg-opacity-70 p-1 text-[1.4rem] transition-all hover:scale-110`}
+            key={index}
+            style={{ cursor: 'pointer' }}
+          >
+            <div className="flex h-full w-full animate-pulse flex-col content-center items-center justify-center rounded-md bg-slate-700">
+              <div className="mb-2 h-6 w-32 animate-pulse rounded bg-gray-400"></div>
+              <div className="mb-2 h-6 w-20 animate-pulse rounded bg-gray-400"></div>
+              <div className="h-6 w-24 animate-pulse rounded bg-gray-400"></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default bento

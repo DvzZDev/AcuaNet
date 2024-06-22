87b39daace86ced22fe21cbef4faa0bac1e64bc1
David <estebandavid578@gmail.com>
2024-06-22 13:28:36 +0200

    Add Divider component for Cuencas page

---
+++ src/components/cuencas/Divider.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Divider() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      data-name="Layer 1"
+      preserveAspectRatio="none"
+      viewBox="0 -1 1200 59"
+    >
+      <g className="layer">
+        <path
+          fill="#070922"
+          d="M0-1l1200 50.9V58H0V-1z"
+          className="shape-fill"
+        ></path>
+      </g>
+    </svg>
+  )
+}
+
+export default Divider

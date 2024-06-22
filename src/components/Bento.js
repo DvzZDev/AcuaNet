bd111df92cfe0903bcc5f22c218dec25b38f6c5e
David <estebandavid578@gmail.com>
2024-06-22 13:27:31 +0200

    Delete console.log

--- src/components/Bento.js
+++ src/components/Bento.js
@@ -29,7 +29,6 @@ async function Bento() {
   const variacionEmbalses = await FetchEmbalsesVariacion()
   const esp = await FetchEsp()
   const pluvis = await FetchPluvis()
-  console.log(pluvis)
 
   return (
     <section

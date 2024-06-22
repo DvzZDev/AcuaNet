81f59e28bd162c2f3b02968c2c1be66f827e4bac
David <estebandavid578@gmail.com>
2024-06-22 13:26:57 +0200

    Refactor Page

--- src/app/cuencas/[cuencaid]/page.js
+++ src/app/cuencas/[cuencaid]/page.js
@@ -1,29 +1,35 @@
-import { FetchCuencas } from '@/lib/data'
-import React from 'react'
+import { FetchCuencas, FetchEmbalses } from '@/lib/data'
+import BentoDist from '@/components/cuencas/BentoDist'
+import Divider from '@/components/cuencas/Divider'
 
 async function Page({ params }) {
-  const data = await FetchCuencas()
+  const cuenca = await FetchCuencas()
+  const embalses = await FetchEmbalses()
   const { cuencaid } = params
   const decodedCuencaid = decodeURIComponent(cuencaid)
 
-  const result = data.find((item) => item.cuenca === decodedCuencaid)
+  const resCuenca = cuenca.find((cuenca) => cuenca.cuenca === decodedCuencaid)
+  const resEmbalses = embalses.filter(
+    (embalse) => embalse.nombre_cuenca === decodedCuencaid
+  )
 
   return (
     <>
-      <div className="flex justify-center">
+      <div className="justify-center">
         <h1 className="mb-2 text-center font-telma text-[2.5rem] text-textprimary sm:mt-10 sm:text-6xl">
-          {result.cuenca}
+          {resCuenca.cuenca}
         </h1>
+
+        <Divider />
       </div>
 
-      <div className="h-screen bg-[#070922]">
-        <h1></h1>
-        <h1>{result.capacidad}</h1>
-        <h1>{result.emabalsada}</h1>
-        <h1>{result.variacion}</h1>
-        <h1>{result.porcentaje_variacion}</h1>
-        <h1>{result.porcentaje_embalsada}</h1>
-      </div>
+      <div className="bg-[#070922]"></div>
+
+      <section className="flex h-full items-center justify-center bg-[#070922] sm:h-[80svh]">
+        <div className="min-h-full">
+          <BentoDist {...resCuenca} />
+        </div>
+      </section>
     </>
   )
 }

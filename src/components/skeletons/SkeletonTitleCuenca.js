75700ec5441d8477b52710d00ece94b4b1322303
David <estebandavid578@gmail.com>
2024-06-24 20:38:10 +0200

    Create new skeleton for Title Component

---
+++ src/components/skeletons/SkeletonTitleCuenca.js
@@ -0,0 +1,11 @@
+function SkeletonTitleCuencas() {
+  return (
+    <div className="justify-center">
+      <h1 className="mb-2 text-center font-telma text-[2.5rem] text-textprimary sm:mt-10 sm:text-6xl">
+        <div className="h-14 w-[30rem] rounded-md bg-slate-400 animate-pulse m-auto"></div>
+      </h1>
+    </div>
+  )
+}
+
+export default SkeletonTitleCuencas

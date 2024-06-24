875a03d4033b902465a786b7fee5bf54532ce3d0
David <estebandavid578@gmail.com>
2024-06-24 20:37:57 +0200

    Change Color

--- src/components/landing/Intro.js
+++ src/components/landing/Intro.js
@@ -9,7 +9,7 @@ function Intro() {
         className="relative flex h-screen animate-fade-up flex-col items-center pt-12 animate-once animate-ease-in-out md:pt-20"
       >
         <div className="flex flex-col xl:w-[60%]">
-          <strong className="text-textsecondary text-center font-telma text-[5rem] sm:text-[7rem]">
+          <strong className="text-center font-telma text-[5rem] text-textsecondary sm:text-[7rem]">
             AcuaEs
           </strong>
           <h1 className="px-6 text-center text-[2rem] text-[#fbffb8] lg:text-[3rem]">
@@ -18,7 +18,7 @@ function Intro() {
           </h1>
         </div>
 
-        <form className="z-10 my-14 flex max-h-16 w-[17rem] justify-between rounded-xl border-2 border-solid border-slate-400 p-1 text-sm transition-all focus-within:border-blue-300 sm:w-[20rem] sm:text-base md:w-[25rem] md:text-base">
+        <form className="md:text-base< z-10 my-14 flex max-h-16 w-[17rem] justify-between rounded-xl border-2 border-solid border-slate-400 p-1 text-sm transition-all focus-within:border-blue-300 sm:w-[20rem] sm:text-base md:w-[25rem]">
           <Type />
           <button className="text-slate-400">Buscar</button>
         </form>

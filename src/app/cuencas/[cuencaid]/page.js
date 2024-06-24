29ec1e8710109728d5426d3f4238bfeaf541d14a
David <estebandavid578@gmail.com>
2024-06-24 20:35:48 +0200

    Fix bug whit Suspense

--- src/app/cuencas/[cuencaid]/page.js
+++ src/app/cuencas/[cuencaid]/page.js
@@ -1,6 +1,7 @@
-import { FetchCuencas, FetchEmbalses } from '@/lib/data'
-import BentoDist from '@/components/cuencas/BentoDist'
+import Title from '@/components/cuencas/Title'
 import Divider from '@/components/cuencas/Divider'
+import BentoDist from '@/components/cuencas/BentoDist'
+import SkeletonTitleCuencas from '@/components/skeletons/SkeletonTitleCuenca'
 import SkeletonBentoDist from '@/components/skeletons/SkeletonBentoDinst'
 import { Suspense } from 'react'
 
@@ -9,30 +10,14 @@ export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
 
 async function Page({ params }) {
-  console.log(params)
-  const cuenca = await FetchCuencas()
-  const embalses = await FetchEmbalses()
-  const { cuencaid } = params
-  const decodedCuencaid = decodeURIComponent(cuencaid)
-
-  const resCuenca = cuenca.find((cuenca) => cuenca.cuenca === decodedCuencaid)
-  const resEmbalses = embalses.filter(
-    (embalse) => embalse.nombre_cuenca === decodedCuencaid
-  )
-
   return (
     <>
-      <div className="justify-center">
-        <h1 className="mb-2 text-center font-telma text-[2.5rem] text-textprimary sm:mt-10 sm:text-6xl">
-          {resCuenca.cuenca}
-        </h1>
-        <Divider />
-      </div>
-
-      <div className="bg-[#070922]"></div>
-
-      <section className="flex h-full items-center justify-center bg-[#070922] sm:h-[80svh]">
-        <div className="min-h-full">
+      <Suspense fallback={<SkeletonTitleCuencas />}>
+        <Title url={params} />
+      </Suspense>
+      <Divider />
+      <section className="flex h-full items-center justify-center bg-[#070922] sm:h-[60svh] lg:h-[80svh]">
+        <div className="flex min-h-fit w-screen justify-center">
           <Suspense fallback={<SkeletonBentoDist />}>
             <BentoDist url={params} />
           </Suspense>

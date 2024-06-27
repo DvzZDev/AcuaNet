a5a32085a6a3af2361046b40afc0e617b6b1ca9a
David <estebandavid578@gmail.com>
2024-06-27 04:41:12 +0200

    refactor: Remove unused next/image import

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Bento from '@/components/landing/Bento'
 import Fuentes from '@/components/landing/Fuentes'
 import SkeletonBento from '@/components/skeletons/BentoSkeleton'
 import { Suspense } from 'react'
-import Image from 'next/image'
 
 export const revalidate = 60
 export const dynamic = 'force-dynamic'

74c34789b597529ae20a5e189e0a2468c3188efc
David <estebandavid578@gmail.com>
2024-06-28 09:12:50 +0200

    Update NotFound component to use lazy loading for the image

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -6,17 +6,18 @@ export default function NotFound() {
     <div className="flex h-screen flex-col items-center justify-center bg-slate-950 text-gray-800">
       <h1 className="mb-4 text-6xl font-bold">404</h1>
       <p className="mb-8 text-2xl">¡Oh no! Parece que te has perdido.</p>
-      <div className='h-[50%] w-[50%]'></div>
-      <Image
-        src="https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif"
-        alt="Funny GIF"
-        className="mb-8 h-64 w-64"
-        layout='fill'
-      />
+      <div className="h-[50%] w-[50%]">
+        <Image
+          src="https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif"
+          alt="Funny GIF"
+          className="h-full w-full object-cover"
+          layout="fill"
+        />
+      </div>
       <Link href="/">
-        <span className="text-lg text-blue-500 hover:underline">
+        <p className="text-lg text-blue-500 hover:underline">
           Volver a la página principal
-        </span>
+        </p>
       </Link>
     </div>
   )

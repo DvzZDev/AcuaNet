c6310536458baf4f11e4a5fb468e83da79f1a9c3
David <estebandavid578@gmail.com>
2024-07-19 14:08:34 +0200

    Update EmbalseContent component styling and layout

--- src/components/skeletons/SkeeltonBentoEmbalseDist.js
+++ src/components/skeletons/SkeeltonBentoEmbalseDist.js
@@ -8,7 +8,7 @@ function EmbalseContent({ data }) {
       <div className="font-NecoBold m-auto h-14 w-[15rem] animate-pulse rounded-lg bg-slate-700 text-center text-[2.5rem] text-textsecondary sm:mt-10 sm:w-[20rem] sm:text-6xl lg:w-[30rem]"></div>
       <Divider />
 
-      <section className="mb-5 flex min-h-[40rem] justify-center bg-bgcolor p-4 text-white lg:pt-1">
+      <section className="mb-5 flex min-h-screen justify-center bg-bgcolor p-4 text-white lg:pt-1">
         <div className="mx-4 flex min-h-full w-full animate-pulse flex-col gap-4 transition-all sm:grid sm:w-[40rem] sm:grid-cols-8 sm:grid-rows-8 lg:h-[40rem] lg:w-[60rem]">
           {/* Primera Columna */}
 

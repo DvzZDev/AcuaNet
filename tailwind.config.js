705567036483f223ce046bf54ad3c4585288dc21
DvzZ <estebandavid578@gmail.com>
2024-10-11 06:57:54 +0200

    Add purgue

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,3 +57,20 @@ export const experimental = {
 }
 
 export const darkMode = "class"
+
+// Añadir la configuración de purge
+export const purge = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(modal|button|image|input|spinner|dropdown|accordion).js",
+  ],
+  safelist: [
+    "text-blue-900",
+    "text-green-900",
+    "text-yellow-900",
+    "text-orange-950",
+    "text-red-900",
+  ],
+}

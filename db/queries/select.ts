0cb012143079cc112a843d47395b79d93ba841e9
DvzZ <estebandavid578@gmail.com>
2024-10-11 02:39:58 +0200

    Add more Data

--- db/queries/select.ts
+++ db/queries/select.ts
@@ -1,6 +1,14 @@
 import { db } from "db"
-import { Embalses } from "db/schema"
+import { Cuencas, España, Embalses } from "db/schema"
 
-export async function Cuencas() {
+export async function GetCuencas() {
+  return db.select().from(Cuencas)
+}
+
+export async function GetEsp() {
+  return db.select().from(España)
+}
+
+export async function GetEmbalses() {
   return db.select().from(Embalses)
 }

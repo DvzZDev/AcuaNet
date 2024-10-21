ff7691b4f42a9d023ce7706d18741186d9d8af9a
DvzZ <estebandavid578@gmail.com>
2024-10-21 21:43:18 +0200

    Fix issue solid input in IOS

--- src/components/landing/SerchEmb.tsx
+++ src/components/landing/SerchEmb.tsx
@@ -24,7 +24,7 @@ export default function SerchEmb({ data }: TypeProps) {
     <div>
       <form
         onSubmit={handleSubmit}
-        className="bg-[#3f7b7648] relative z-10 mt-8 flex max-h-16 w-[15rem] items-center justify-center rounded-2xl border border-solid border-green-100 p-1 text-sm transition-all focus-within:border-green-200 sm:w-[20rem] sm:text-base md:w-[20rem] md:text-xl"
+        className="relative z-10 mt-8 flex max-h-16 w-[15rem] items-center justify-center rounded-2xl border border-solid border-green-100 bg-green-100/20 p-1 text-sm transition-all focus-within:border-green-200 sm:w-[20rem] sm:text-base md:w-[20rem] md:text-xl"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"

dd81aa777012f3659d75c7b77bb2d24f55898fbe
DvzZ <estebandavid578@gmail.com>
2024-10-23 02:45:31 +0200

    To upper case

--- src/components/embalses/TitleEmb.tsx
+++ src/components/embalses/TitleEmb.tsx
@@ -1,8 +1,11 @@
 async function TitleEmb({ data }: { data: { nombre_embalse: string } }) {
+  const capitalizedNombreEmbalse = data.nombre_embalse.replace(/\b\w/g, (char) =>
+    char.toUpperCase()
+  )
   return (
     <div className="flex justify-center">
-      <h1 className="mt-4 text-center text-[2.5rem] font-bold xl:mb-0 text-green-100 sm:mb-2 sm:mt-10 sm:text-6xl">
-        {data.nombre_embalse}
+      <h1 className="mt-4 text-center text-[2.5rem] font-bold text-green-100 sm:mb-2 sm:mt-10 sm:text-6xl xl:mb-0">
+        {capitalizedNombreEmbalse}
       </h1>
     </div>
   )

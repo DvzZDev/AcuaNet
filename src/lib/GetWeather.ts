ad88bfbae32e10558811f99676f50668dbb33556
DvzZ <estebandavid578@gmail.com>
2024-11-21 02:51:04 +0100

    Refactor GetWeather function to accept latitude and longitude as parameters; update API URL construction

--- src/lib/GetWeather.ts
+++ src/lib/GetWeather.ts
@@ -1,6 +1,8 @@
 import { WeatherTypes } from "@/types"
 
-export default async function GetWeather(url: string) {
+export default async function GetWeather(lat: number, lon: number) {
+  const key = process.env.NEXT_PUBLIC_WEATHER_API_KEY
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}?unitGroup=metric&include=days%2Chours&key=${key}&contentType=json&iconSet=icons2`
   const response = await fetch(url)
   const weatherData: WeatherTypes = await response.json()
   return weatherData

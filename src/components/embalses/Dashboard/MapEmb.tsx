9540e6d466f2b339bd5595b9e451ccb6f4e5b953
DvzZ <estebandavid578@gmail.com>
2024-11-23 05:27:05 +0100

    Enhance MapEmb component styling for better visibility of coordinates

--- src/components/embalses/Dashboard/MapEmb.tsx
+++ src/components/embalses/Dashboard/MapEmb.tsx
@@ -12,8 +12,10 @@ export default function MapEmb({ coords }: { coords: Coords }) {
   return (
     <>
       <h3 className="text-2xl font-black text-green-950">Mapa</h3>
-      <section className="relative aspect-auto h-fit w-full overflow-hidden rounded-lg border border-green-900/30 bg-green-100">
-        <p className="absolute bottom-2 right-2 z-10 text-xs font-semibold text-green-100 md:text-base">{coords.name}</p>
+      <section className="relative aspect-auto h-fit w-full overflow-hidden rounded-lg border-green-900/30 bg-green-100">
+        <p className="absolute bottom-0 right-0 z-10 rounded-lg bg-[#052e16] p-2 text-right text-[10px] font-semibold text-green-100 md:text-base">
+          {coords.name}
+        </p>
         <MapContainer
           className="map-container"
           center={[coords.lat, coords.lon]}

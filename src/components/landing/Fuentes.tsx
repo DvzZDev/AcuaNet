59fa9c168814ba26f78d00f7a5cb6e889630f5e3
DvzZ <estebandavid578@gmail.com>
2024-11-23 05:59:01 +0100

    Replace Image component with img tags in Fuentes component for improved performance and simplicity

--- src/components/landing/Fuentes.tsx
+++ src/components/landing/Fuentes.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Image from "next/image"
 
 function Fuentes() {
   return (
@@ -10,23 +9,20 @@ function Fuentes() {
       <h4 className="text-center text-[2.3rem] font-black leading-none text-green-100 sm:text-[50px]">Fuentes Oficiales</h4>
       <h4 className="mt-3 text-center text-[20px] text-green-200">Datos actualizdos cada 24 horas.</h4>
       <div className="mt-8 flex flex-col gap-7 md:flex-row md:gap-16">
-        <Image
+        <img
           src="/aemet.webp"
           alt="Aemet España"
-          width={200}
-          height={200}
+          className="h-auto w-full max-w-[200px]"
         />
-        <Image
+        <img
           src="/miteco.webp"
-          alt="Aemet España"
-          width={200}
-          height={200}
+          alt="Miteco España"
+          className="h-auto w-full max-w-[200px]"
         />
-        <Image
+        <img
           src="/sahi.webp"
-          alt="Aemet España"
-          width={200}
-          height={200}
+          alt="Sahi España"
+          className="h-auto w-full max-w-[200px]"
         />
       </div>
     </section>

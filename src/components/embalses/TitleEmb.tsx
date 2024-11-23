75279286b7b72f84ed23f28dc065d2aa91a01a88
DvzZ <estebandavid578@gmail.com>
2024-11-23 05:27:13 +0100

    Enhance TitleEmb component to support multiple data structures and add Divider for improved layout

--- src/components/embalses/TitleEmb.tsx
+++ src/components/embalses/TitleEmb.tsx
@@ -1,11 +1,26 @@
-function TitleEmb({ data }: { data: { nombre_embalse: string } }) {
-  const capitalizedNombreEmbalse = data.nombre_embalse.replace(/\b\w/g, (char) => char.toUpperCase())
+import Divider from "../landing/Divider"
+
+function TitleEmb({
+  data,
+}: {
+  data: { nombre_embalse: string; cuenca_id: string } | { nombre_embalse: string } | { cuenca_id: string }
+}) {
+  const capitalizedNombreEmbalse =
+    "nombre_embalse" in data
+      ? data.nombre_embalse.replace(/\b\w/g, (char) => char.toUpperCase())
+      : "cuenca_id" in data
+        ? data.cuenca_id.replace(/\b\w/g, (char) => char.toUpperCase())
+        : ""
+
   return (
-    <div className="flex justify-center">
-      <h1 className="mt-4 text-center text-[2.5rem] font-bold text-green-100 sm:mb-2 sm:mt-10 sm:text-6xl xl:mb-0">
-        {capitalizedNombreEmbalse}
-      </h1>
-    </div>
+    <>
+      <div className="flex justify-center">
+        <h1 className="mt-4 text-center text-[2.5rem] font-bold text-green-100 sm:mb-2 sm:mt-10 sm:text-6xl xl:mb-0">
+          {capitalizedNombreEmbalse}
+        </h1>
+      </div>
+      <Divider />
+    </>
   )
 }
 

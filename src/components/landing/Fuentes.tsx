c1f960950e46b7d3f5c03d2448fc50f4d84a8f14
DvzZ <estebandavid578@gmail.com>
2024-11-24 22:46:34 +0100

    Update Fuentes component: adjust image styling for consistency and prevent dragging

--- src/components/landing/Fuentes.tsx
+++ src/components/landing/Fuentes.tsx
@@ -8,21 +8,24 @@ function Fuentes() {
     >
       <h2 className="text-center text-[2.3rem] font-black leading-none text-green-100 sm:text-[50px]">Fuentes Oficiales</h2>
       <h2 className="mt-3 text-center text-[20px] text-green-200">Datos actualizdos cada 24 horas.</h2>
-      <div className="mt-8 flex flex-col gap-7 md:flex-row md:gap-16">
+      <div className="mt-8 flex flex-col md:flex-row md:gap-16">
         <img
           src="/aemet.webp"
           alt="Aemet España"
-          className="h-auto w-full max-w-[200px]"
+          className="h-[120px] w-[200px] object-contain"
+          draggable="false"
         />
         <img
           src="/miteco.webp"
           alt="Miteco España"
-          className="h-auto w-full max-w-[200px]"
+          className="h-[120px] w-[200px] object-contain"
+          draggable="false"
         />
         <img
           src="/sahi.webp"
           alt="Sahi España"
-          className="h-auto w-full max-w-[200px]"
+          className="h-[120px] w-[200px] object-contain"
+          draggable="false"
         />
       </div>
     </section>

53f962b80bb3bec03e318e191bc52e75c07203e9
DvzZ <estebandavid578@gmail.com>
2024-12-07 19:51:53 +0100

    Add snowfall effect and update styles for holiday theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,13 @@ import { Providers } from "./providers"
 import Navbar from "@/components/global/Navbar"
 import PostHogPageView from "./PostHogPageView"
 import { Suspense } from "react"
-
+import Snow from "@/components/landing/Snow"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <head></head>
       <body>
         <Providers>
+          <Snow />
           <header className="h-[3.6rem]">
             <Navbar />
           </header>

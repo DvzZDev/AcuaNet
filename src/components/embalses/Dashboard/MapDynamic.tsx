84935a4743afc0ceaca45e659b1e98c610e532d8
DvzZ <estebandavid578@gmail.com>
2024-12-08 03:49:59 +0100

    Refactor map components to improve structure and replace MapEmb with iframe for Windy integration

--- src/components/embalses/Dashboard/MapDynamic.tsx
+++ src/components/embalses/Dashboard/MapDynamic.tsx
@@ -2,14 +2,7 @@
 
 import dynamic from "next/dynamic"
 
-const MapDynamic = dynamic(() => import("@/components/embalses/Dashboard/MapEmb"), {
-  ssr: false,
-  loading: () => (
-    <section className="relative aspect-auto h-[400px] w-full overflow-hidden rounded-lg border border-green-900/30 bg-green-100"></section>
-  ),
-})
-
-const MapDynamic2 = dynamic(() => import("@/components/embalses/Dashboard/MapClose"), {
+const MapaCercano = dynamic(() => import("@/components/embalses/Dashboard/MapaCercano"), {
   ssr: false,
   loading: () => (
     <section className="relative aspect-auto h-[400px] w-full overflow-hidden rounded-lg border border-green-900/30 bg-green-100"></section>
@@ -26,16 +19,19 @@ export default function MapEmbData({ coords }: { coords: { lat: number; lon: num
       <h3 className="text-2xl font-black text-green-950">Mapas</h3>
       <section className="grid gap-4 md:grid-cols-2">
         <div className="col-span-1">
-          <MapDynamic2
+          <MapaCercano
             key={`${coords.lat}-${coords.lon}-${coords.name}`}
             coords={{ lat: coords.lat, lon: coords.lon, name: coords.name }}
           />
         </div>
         <div className="col-span-1">
-          <MapDynamic
-            key={`${coords.lat}-${coords.lon}`}
-            coords={{ lat: coords.lat, lon: coords.lon }}
-          />
+          <iframe
+            width="100%"
+            height="400"
+            className="rounded-lg"
+            loading="lazy"
+            src={`https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=mm&metricTemp=°C&metricWind=km/h&zoom=11&overlay=wind&product=ecmwf&level=surface&lat=${coords.lat}&lon=${coords.lon}&detailLat=${coords.lat}&detailLon=${coords.lon}&marker=true&message=true`}
+          ></iframe>
         </div>
       </section>
     </>

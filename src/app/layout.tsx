61a53d4a2f528fee7838f4b9287172ed58127141
DvzZ <estebandavid578@gmail.com>
2024-12-29 00:52:37 +0100

    Add structured data for SEO in layout head section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,30 @@ import Navbar from "@/components/global/Navbar"
 import PostHogPageView from "./PostHogPageView"
 import { Suspense } from "react"
 import Snow from "@/components/landing/Snow"
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        <script type="application/ld+json">
+          {`
+        {
+          "@context": "https://schema.org",
+          "@type": "WebSite",
+          "name": "AcuaNet",
+          "url": "https://acuanet.es",
+          "description": "AcuaNet es una web dedicada a los pescadores donde pueden visualizar fácilmente el nivel de los embalses y cuencas, tablas lunares y pronóstico del tiempo, todo en uno para facilitar sus jornadas de pesca.",
+          "foundingDate": "2024-06",
+          "potentialAction": {
+            "@type": "SearchAction",
+            "target": "https://acuanet.es/search?q={search_term_string}",
+            "query-input": "required name=search_term_string"
+          },
+         "keywords": "pesca, nivel de los embalses, agrbaits, pesca deportiva, jornadas de pesca, planificacion jornadas de pesca, embalses, cuencas, tablas lunares, nivel cuencas españa, acuanet, acuanet.es"
+        }
+          `}
+        </script>
+      </head>
       <body>
         <Providers>
           <Snow />

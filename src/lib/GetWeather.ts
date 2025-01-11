4ec113799e13535fa1857ce1c56cdc9a74ad369b
DvzZ <estebandavid578@gmail.com>
2025-01-12 00:58:06 +0100

    Update weather API URL to include additional elements and improve data retrieval

--- src/lib/GetWeather.ts
+++ src/lib/GetWeather.ts
@@ -4,7 +4,7 @@ export default async function GetWeather(lat: number, lon: number) {
   const key = process.env.NEXT_PUBLIC_WEATHER_API_KEY
   // const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}?unitGroup=metric&include=days%2Chours&key=${key}&contentType=json&iconSet=icons2`
 
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}?unitGroup=metric&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp%2Cprecip%2Cprecipprob%2Cwindspeed%2Cwindspeedmax%2Cwindspeedmin%2Cwinddir%2Cpressure%2Cicon&include=days%2Chours&key=${key}&options=nonulls&contentType=json`
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}?unitGroup=metric&next7days?elements=datetime%2Ctempmax%2Ctempmin%2Ctemp%2Cwindgust%2Cwindspeed%2Cwindspeedmax%2Cwindspeedmean%2Cwindspeedmin%2Cwinddir%2Cpressure&include=hours%2Cdays%2Cfcst%2Cstatsfcst&key=${key}&contentType=json&iconSet=icons2`
 
   const response = await fetch(url, {
     next: {
@@ -13,5 +13,6 @@ export default async function GetWeather(lat: number, lon: number) {
   })
 
   const weatherData: WeatherTypes = await response.json()
+  console.log(weatherData)
   return weatherData
 }

e3f8d665567902806ff8bb04edb5cdd16dc806a4
DvzZ <estebandavid578@gmail.com>
2025-01-18 12:35:34 +0100

    Remove caching options from weather data fetch in GetWeather function

--- src/lib/GetWeather.ts
+++ src/lib/GetWeather.ts
@@ -6,11 +6,7 @@ export default async function GetWeather(lat: number, lon: number) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/next7days?unitGroup=metric&elements=datetime%2Cname%2CresolvedAddress%2Ctempmax%2Ctempmin%2Ctemp%2Cwindgust%2Cwindspeed%2Cwindspeedmax%2Cwindspeedmean%2Cwindspeedmin%2Cwinddir%2Cpressure%2Cicon%2Csource&include=current%2Cdays%2Chours&key=${key}&options=nonulls&contentType=json&iconSet=icons2
 `
 
-  const response = await fetch(url, {
-    next: {
-      revalidate: 43200, // Cache for 12 hours
-    },
-  })
+  const response = await fetch(url, {})
 
   const weatherData: WeatherTypes = await response.json()
   return weatherData

1422d9fb6fa1149652087d81a399b0fede02fff6
DvzZ <estebandavid578@gmail.com>
2025-01-20 22:22:39 +0100

    Add web app manifest and icons for improved PWA support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,33 +12,30 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <head>
-        <head>
-          <head>
-            <Script
-              id="structured-data"
-              strategy="beforeInteractive"
-              type="application/ld+json"
-              dangerouslySetInnerHTML={{
-                __html: JSON.stringify({
-                  "@context": "https://schema.org",
-                  "@type": "WebSite",
-                  name: "AcuaNet",
-                  url: "https://acuanet.es",
-                  description:
-                    "AcuaNet es una web dedicada a los pescadores donde pueden visualizar fácilmente el nivel de los embalses y cuencas, tablas lunares y pronóstico del tiempo, todo en uno para facilitar sus jornadas de pesca.",
-                  foundingDate: "2024-06",
-                  potentialAction: {
-                    "@type": "SearchAction",
-                    target: "https://acuanet.es/search?q={search_term_string}",
-                    "query-input": "required name=search_term_string",
-                  },
-                  keywords:
-                    "pesca, nivel de los embalses, agrbaits, pesca deportiva, jornadas de pesca, planificacion jornadas de pesca, embalses, cuencas, tablas lunares, nivel cuencas españa, acuanet, acuanet.es",
-                }),
-              }}
-            />
-          </head>
-        </head>
+        <Script
+          id="structured-data"
+          strategy="beforeInteractive"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "AcuaNet",
+              url: "https://acuanet.es",
+              description:
+                "AcuaNet es una web dedicada a los pescadores donde pueden visualizar fácilmente el nivel de los embalses y cuencas, tablas lunares y pronóstico del tiempo, todo en uno para facilitar sus jornadas de pesca.",
+              foundingDate: "2024-06",
+              potentialAction: {
+                "@type": "SearchAction",
+                target: "https://acuanet.es/search?q={search_term_string}",
+                "query-input": "required name=search_term_string",
+              },
+              keywords:
+                "pesca, nivel de los embalses, agrbaits, pesca deportiva, jornadas de pesca, planificacion jornadas de pesca, embalses, cuencas, tablas lunares, nivel cuencas españa, acuanet, acuanet.es",
+            }),
+          }}
+        />
+        <meta charSet="utf-8" />
       </head>
       <body>
         <Providers>

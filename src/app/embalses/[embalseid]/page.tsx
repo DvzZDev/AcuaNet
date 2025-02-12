08e44c8b8f9ea1c1f7eb4a2a6f1038855af9bc04
DvzZ <estebandavid578@gmail.com>
2025-02-13 00:24:18 +0100

    Refactor Page component to simplify props structure and enhance parameter handling

--- src/app/embalses/[embalseid]/page.tsx
+++ src/app/embalses/[embalseid]/page.tsx
@@ -20,12 +20,8 @@ import FilterHistoricalData from "@/lib/FilterHistoricalData"
 import Chart from "@/components/embalses/Dashboard/Chart"
 // import Names from "@/lib/nombresEmbalses.json"
 
-interface PageProps {
-  params: { embalseid: string }
-}
-
-async function Page({ params }: PageProps) {
-  const { embalseid } = params
+async function Page({ params }: { params: { embalseid: string } }) {
+  const { embalseid } = await params
   const decodedEmbalseid = embalseid.replace(/-/g, " ")
   const embalses = (await GetHistoricalData(decodedEmbalseid)) as Embalses[]
   const pActual = embalses[0].porcentaje

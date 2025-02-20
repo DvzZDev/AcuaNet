b17833bb91b8545ce63161ecc72eafc130cb7103
DvzZ <estebandavid578@gmail.com>
2025-02-20 15:40:59 +0100

    Refactor legal page layout; update headings for consistency and improve styling

--- src/app/legal/page.tsx
+++ src/app/legal/page.tsx
@@ -33,79 +33,75 @@ function page() {
   return (
     <>
       <Intro title="Aviso Legal" />
-      <section className="flex justify-center bg-green-50 text-green-950">
-        <div
-          id="pp"
-          className="container mt-2 rounded-lg p-4 shadow-md"
-        >
-          <h1 className="mb-4 text-2xl font-bold text-green-700">Aviso Legal</h1>
+      <section className="flex justify-center bg-green-50 px-5 py-8 text-green-950">
+        <div className="container w-[75ch] space-y-4 rounded-xl shadow-md">
           <p>
             <strong>Propietario del sitio web:</strong>
             <br /> David Esteban García
             <br />
             España
           </p>
-          <h2 className="mt-4 mb-2 text-xl font-semibold text-green-700">Condiciones de uso:</h2>
+          <h2 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Condiciones de uso:</h2>
           <p>
             El acceso y uso de este sitio web implica la aceptación plena y sin reservas de las condiciones aquí establecidas. En
             caso de desacuerdo con alguna de estas condiciones, por favor no utilices el sitio.
           </p>
-          <h2 className="mt-4 mb-2 text-xl font-semibold text-green-700">Propiedad intelectual:</h2>
+          <h2 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Propiedad intelectual:</h2>
           <p>
             Todos los contenidos de este sitio web, incluyendo textos, gráficos, imágenes y su diseño, son propiedad de David
             Esteban García o de sus legítimos propietarios. Quedan reservados todos los derechos sobre los mismos.
           </p>
-          <h2 className="mt-4 mb-2 text-xl font-semibold text-green-700">Protección de datos:</h2>
+          <h2 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Protección de datos:</h2>
           <p>
             Este sitio web utiliza herramientas para analizar las visitas. Todos los demás datos del sitio son de código abierto.
             Los datos personales proporcionados a través de formularios serán tratados conforme a la normativa vigente en materia
             de protección de datos personales.
           </p>
-          <h3 className="mt-4 mb-2 text-lg font-semibold text-green-700">Derechos del usuario:</h3>
+          <h3 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Derechos del usuario:</h3>
           <p>
             Tienes derecho a acceder, rectificar y suprimir tus datos personales, así como a limitar u oponerte a su tratamiento.
             También tienes derecho a la portabilidad de tus datos. Para ejercer estos derechos, puedes contactar a David Esteban
             García a través del correo electrónico proporcionado.
           </p>
-          <h3 className="mt-4 mb-2 text-lg font-semibold text-green-700">Finalidad del tratamiento de datos:</h3>
+          <h3 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Finalidad del tratamiento de datos:</h3>
           <p>
             Los datos recopilados se utilizan exclusivamente para analizar las visitas al sitio web y mejorar la experiencia del
             usuario. No se comparten con terceros, salvo obligación legal. Además, el formulario de contacto guarda los datos de
             la persona que ha contactado y su correo electrónico, utilizando Cloudflare Turnstile para prevenir envíos
             automáticos.
           </p>
-          <h3 className="mt-4 mb-2 text-lg font-semibold text-green-700">Medidas de seguridad:</h3>
+          <h3 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Medidas de seguridad:</h3>
           <p>
             Se han implementado las medidas técnicas y organizativas necesarias para garantizar la seguridad de tus datos
             personales y evitar su alteración, pérdida, tratamiento o acceso no autorizado. Esto incluye el uso de cifrado SSL en
             la transmisión de datos, el mantenimiento de servidores seguros y el uso de cookies de seguridad proporcionadas por
             Cloudflare para proteger el sitio web.
           </p>
-          <h2 className="mt-4 mb-2 text-xl font-semibold text-green-700">Exclusión de garantías y responsabilidad:</h2>
+          <h2 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Exclusión de garantías y responsabilidad:</h2>
           <p>
             David Esteban García no se responsabiliza de los daños y perjuicios de cualquier naturaleza que puedan derivarse del
             uso del sitio web, incluyendo errores u omisiones en los contenidos, falta de disponibilidad del sitio, o transmisión
             de virus.
           </p>
-          <h2 className="mt-4 mb-2 text-xl font-semibold text-green-700">Modificaciones:</h2>
+          <h2 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Modificaciones:</h2>
           <p>
             David Esteban García se reserva el derecho de efectuar sin previo aviso las modificaciones que considere oportunas en
             el sitio web, pudiendo cambiar, suprimir o añadir contenidos y servicios.
           </p>
-          <h2 className="mt-4 mb-2 text-xl font-semibold text-green-700">Enlaces:</h2>
+          <h2 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Enlaces:</h2>
           <p>
             Este sitio web puede contener enlaces a otros sitios de Internet. David Esteban García no ejerce ningún control sobre
             estos sitios y no asume responsabilidad alguna por sus contenidos o servicios.
           </p>
-          <h2 className="mt-4 mb-2 text-xl font-semibold text-green-700">Derecho de exclusión:</h2>
+          <h2 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Derecho de exclusión:</h2>
           <p>
             David Esteban García se reserva el derecho a denegar o retirar el acceso al sitio y/o los servicios ofrecidos sin
             necesidad de preaviso, a instancia propia o de un tercero, a aquellos usuarios que incumplan las presentes Condiciones
             Generales de Uso.
           </p>
-          <h2 className="mt-4 mb-2 text-xl font-semibold text-green-700">Código abierto:</h2>
+          <h2 className="text-xl font-semibold text-emerald-700 lg:text-2xl">Código abierto:</h2>
           <p>
-            El código fuente de este sitio web está disponible en el siguiente repositorio de GitHub:
+            El código fuente de este sitio web está disponible en el siguiente repositorio de GitHub:{" "}
             <a
               href="https://github.com/DvzZDev/ACUA-Landing"
               className="text-blue-500 underline"

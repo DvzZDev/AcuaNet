9fae8b4f0e98549b7421a08a384f4d2af08a096f
DvzZ <estebandavid578@gmail.com>
2025-02-21 00:19:01 +0100

    Refactor sitemap configuration to use names.json for improved path generation

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,29 +1,20 @@
-import GetCuencas from "./src/lib/Cuencas.json" assert { type: "json" }
-import GetEmbalses from "./src/lib/Embalses.json" assert { type: "json" }
+import Names from "./src/lib/names.json" assert { type: "json" }
 
 const sitemapConfig = {
   siteUrl: "https://www.acuanet.es/",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   additionalPaths: async () => {
-    const DataCuencas = Array.isArray(GetCuencas) ? GetCuencas : []
-    const DataEmbalses = Array.isArray(GetEmbalses.embalses) ? GetEmbalses.embalses : []
+    const DataNames = Array.isArray(Names) ? Names : []
 
-    const pathsCuencas = DataCuencas.map((cuenca) => ({
-      loc: `${sitemapConfig.siteUrl}cuencas/${cuenca.cuenca.replace(/_/g, "-").replace(/ /g, "%20")}`,
+    const pathsNames = DataNames.map((name) => ({
+      loc: `${sitemapConfig.siteUrl}embalse/${name.toLowerCase().replace(/ /g, "-")}`,
       changefreq: "daily",
       priority: 0.7,
       lastmod: new Date().toISOString(),
     }))
 
-    const pathsEmbalses = DataEmbalses.map((embalse) => ({
-      loc: `${sitemapConfig.siteUrl}embalses/${embalse.nombre_embalse.replace(/ /g, "%20").toLowerCase()}`,
-      changefreq: "daily",
-      priority: 0.7,
-      lastmod: new Date().toISOString(),
-    }))
-
-    return [...pathsCuencas, ...pathsEmbalses]
+    return [...pathsNames]
   },
 }
 

c4126cb0216c8cc0009cef237f1f08262d656f99
DvzZ <estebandavid578@gmail.com>
2025-10-08 04:28:34 +0200

    feat: add subscriptionType prop to UpperBar component and pass it to AccountBanner

--- src/components/dashboard/Navegation/UpperBar.tsx
+++ src/components/dashboard/Navegation/UpperBar.tsx
@@ -1,14 +1,18 @@
 import { createSvClient } from "@/db/server"
-import { UserData } from "@/types"
+import { SubscriptionType, UserData } from "@/types"
 import { Suspense } from "react"
 import AccountBanner from "./AccountBanner"
 import UpperBarCliMob from "./UpperBarCliMob"
 import UpperBarCliPc from "./UpperBarCliPc"
 
-export default async function UpperBar() {
+interface UpperBarProps {
+  subscriptionType: SubscriptionType
+}
+
+export default async function UpperBar({ subscriptionType }: UpperBarProps) {
   const supabase = await createSvClient()
 
-  const { data, error } = await supabase.auth.getUser()
+  const { data } = await supabase.auth.getUser()
   const user = data?.user || null
 
   return (
@@ -16,7 +20,10 @@ export default async function UpperBar() {
       <div className="relative z-[100] hidden h-20 w-full items-center bg-emerald-50 p-3 md:flex">
         <UpperBarCliPc />
         <Suspense fallback={<div>Loading user info...</div>}>
-          <AccountBanner userdata={user?.user_metadata as UserData} />
+          <AccountBanner
+            userdata={user?.user_metadata as UserData}
+            subscriptionType={subscriptionType}
+          />
         </Suspense>
       </div>
       <div className="relative z-[1100] flex md:hidden">
